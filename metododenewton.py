76e5786394375d1c3f0ffba20729efbdbdbe850f
Paulo Henrique Diniz <ph.diniz@usp.br>
2021-04-07 15:09:45 -0300

    adição código

---
+++ metododenewton.py
@@ -0,0 +1,59 @@
+import numpy as np
+import sympy as sy
+from sympy import *
+
+def MetodoDeNewton(f,Df,x0,epsilon,max_iter):
+    '''Approximate solution of f(x)=0 by Newton's method.
+
+    Parameters
+    ----------
+    f : function
+        Function for which we are searching for a solution f(x)=0.
+    Df : function
+        Derivative of f(x).
+    x0 : number
+        Initial guess for a solution f(x)=0.
+    epsilon : number
+        Stopping criteria is abs(f(x)) < epsilon.
+    max_iter : integer
+        Maximum number of iterations of Newton's method.
+
+    Returns
+    -------
+    xn : number
+        Implement Newton's method: compute the linear approximation
+        of f(x) at xn and find x intercept by the formula
+            x = xn - f(xn)/Df(xn)
+        Continue until abs(f(xn)) < epsilon and return xn.
+        If Df(xn) == 0, return None. If the number of iterations
+        exceeds max_iter, then return None.
+
+    Examples
+    --------
+    >>> f = lambda x: x**2 - x - 1
+    >>> Df = lambda x: 2*x - 1
+    >>> newton(f,Df,1,1e-8,10)
+    Found solution after 5 iterations.
+    1.618033988749989
+    '''
+    xn = x0
+    for n in range(0,max_iter):
+        fxn = f.subs(x, xn).evalf()
+        if abs(fxn) < epsilon:
+            print('Found solution after',n,'iterations.')
+            return xn
+        Dfxn = Df.subs(x, xn).evalf()
+        if Dfxn == 0:
+            print('Zero derivative. No solution found.')
+            return None
+        xn = xn - fxn/Dfxn
+    print('Exceeded maximum iterations. No solution found.')
+    return None
+
+x = sy.Symbol('x')
+def f(x):
+    return x**3 -3  #Expressão
+def df(x):
+    return sy.diff(f(x))
+valorRaiz = MetodoDeNewton(f(x), df(x), -15, 10**-3, 100) #usar 0 como primeiro pode ser ruim.
+print (valorRaiz)
